34b30347b5c97ff90af409c4a246eadadf3c6801
Mustafa Barodawala <24886321+itsjustmustafa@users.noreply.github.com>
2023-12-21 23:12:50 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Use a free IP geolocation API to get the user's location based on their IP
+    fetch('https://ipapi.co/json/')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const resultElement = document.getElementById('result');
+            const state = data.region_code;
+
+            if (state === 'IL') {
+                resultElement.textContent = 'You are in Illinois!';
+            } else {
+                resultElement.textContent = 'You are not in Illinois.';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching location:', error);
+            document.getElementById('result').textContent = 'Unable to determine your location.';
+        });
+});
